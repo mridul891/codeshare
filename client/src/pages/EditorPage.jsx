c9d788cff32626bf771b07a66f8a619c68cd3cb6
mridul891 <58576921+mridul891@users.noreply.github.com>
2024-06-09 21:51:52 +0530

    editor page design Done

--- client/src/pages/EditorPage.jsx
+++ client/src/pages/EditorPage.jsx
@@ -1,5 +1,38 @@
+import { useState } from "react";
+import logo from "../assets/code-sync.png";
+import Client from "../Components/Client";
+import Editor from "../Components/Editor";
+
 const EditorPage = () => {
-  return <div>EditorPage</div>;
+  const [clients, setClients] = useState([
+    { socketId: 1, username: "Mridul" },
+    { socketId: 2, username: "Mridul Pandey" },
+    { socketId: 3, username: "Manu" },
+  ]);
+
+  return (
+    <div className="mainWrap">
+      <div className="aside">
+        <div className="asideInner">
+          <div className="logo">
+            <img src={logo} alt="Logo" className="LogoImg" />
+          </div>
+          <h3>Connected</h3>
+          <div className="clientlist">
+            {clients.map((client) => (
+              <Client username={client.username} key={client.socketId} />
+            ))}
+          </div>
+        </div>
+
+        <button className="btn copyBtn">Copy Room</button>
+        <button className="btn leaveBtn">Leave</button>
+      </div>
+      <div className="editorwrap">
+        <Editor />
+      </div>
+    </div>
+  );
 };
 
 export default EditorPage;
