0c7e00239ecbafebc5daafa3f4f1f41d56166b8f
mridul891 <58576921+mridul891@users.noreply.github.com>
2024-06-10 21:56:02 +0530

    chagnes in origin

--- server/server.js
+++ server/server.js
@@ -10,7 +10,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
     cors: {
-        origin: "http://localhost:5173", // specify the client URL
+        origin: ["http://localhost:5173" ,'https://codeshare-theta.vercel.app/'], // specify the client URL
         methods: ["GET", "POST"],
         credentials: true
     }
