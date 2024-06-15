a386ef9596cc628171775321d9af55a9292986e8
mridul891 <58576921+mridul891@users.noreply.github.com>
2024-06-15 22:32:17 +0530

    changes in server  cors origin

--- server/server.js
+++ server/server.js
@@ -17,7 +17,9 @@ app.get('*', (req, res) => {
 // -----------------------------------------
 const io = new Server(server, {
     cors: {
-        origin: ['https://codeshare-theta.vercel.app', 'http://localhost:5173/'],// specify the client URL
+        // origin: ['https://codeshare-theta.vercel.app', 'http://localhost:5173/'],
+        // specify the client URL
+        origin: 'http://localhost:5173',
         methods: ["GET", "POST"],
         allowedHeaders: "*",
         credentials: true
